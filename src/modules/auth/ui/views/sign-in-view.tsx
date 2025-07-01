324b8294a6dd3e3c52d4733503dd23312a5c3a9d
Rohit Sharma <rohits8731@gmail.com>
2025-07-01 19:56:06 +0530

    06: Dashboard Sidebar

--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -153,7 +153,7 @@ export const SignInView = () => {
                             </div>
                         </form>
                     </Form>
-                    <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+                    <div className="bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center">
                         <Image src={"/logo.svg"} alt="Logo" width={92} height={92}/>
                         <p className="text-2xl font-semibold text-white">
                             Meet.AI
--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -192,7 +192,7 @@ export const SignUpView = () => {
                             </div>
                         </form>
                     </Form>
-                    <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+                    <div className="bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center">
                         <Image src={"/logo.svg"} alt="Logo" width={92} height={92}/>
                         <p className="text-2xl font-semibold text-white">
                             Meet.AI
