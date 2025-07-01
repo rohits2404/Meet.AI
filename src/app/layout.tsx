fd5cdd32ac0890bffed474bff48be5f4d4c33853
Rohit Sharma <rohits8731@gmail.com>
2025-07-02 01:51:03 +0530

    08: tRPC Setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { TRPCReactProvider } from "@/trpc/client";
 import "./globals.css";
 
 const inter = Inter({
@@ -13,12 +14,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <html lang="en">
-            <body
-            className={`${inter.className} antialiased`}
-            >
-                {children}
-            </body>
-        </html>
+        <TRPCReactProvider>
+            <html lang="en">
+                <body
+                className={`${inter.className} antialiased`}
+                >
+                    {children}
+                </body>
+            </html>
+        </TRPCReactProvider>
     );
 }
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,30 +1,18 @@
 "use client";
 
-import { Button } from '@/components/ui/button'
-import { authClient } from '@/lib/auth-client'
-import { useRouter } from 'next/navigation';
+import { useTRPC } from '@/trpc/client';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react'
 
 export const HomeView = () => {
 
-    const router = useRouter();
+    const trpc = useTRPC();
 
-    const { data: session } = authClient.useSession();
-
-    if(!session){
-        return(
-            <p>Loading...</p>
-        )
-    }
+    const { data } = useQuery(trpc.hello.queryOptions({ text: "Rohit" }))
 
     return(
         <div className='flex flex-col p-4 gap-y-4'>
-            <p>Logged In as {session.user.name}</p>
-            <Button onClick={()=>authClient.signOut({ 
-                fetchOptions: { onSuccess: () => router.push("/sign-in")}
-            })}>
-                Sign Out
-            </Button>
+            {data?.greeting}
         </div>
     )
 }
