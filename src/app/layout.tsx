935f2c3cc3a4fef73e0c5768bc20021d7e74189f
Rohit Sharma <rohits8731@gmail.com>
2025-07-20 13:58:23 +0530

    13: Agents Filters

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { TRPCReactProvider } from "@/trpc/client";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { NuqsAdapter } from "nuqs/adapters/next";
 
 const inter = Inter({
     subsets: ["latin"],
@@ -15,15 +16,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <TRPCReactProvider>
-            <html lang="en">
-                <body
-                className={`${inter.className} antialiased`}
-                >
-                    <Toaster/>
-                    {children}
-                </body>
-            </html>
-        </TRPCReactProvider>
+        <NuqsAdapter>
+            <TRPCReactProvider>
+                <html lang="en">
+                    <body
+                    className={`${inter.className} antialiased`}
+                    >
+                        <Toaster/>
+                        {children}
+                    </body>
+                </html>
+            </TRPCReactProvider>
+        </NuqsAdapter>
     );
 }
--- src/modules/agents/ui/components/agents-list-header.tsx
+++ src/modules/agents/ui/components/agents-list-header.tsx
@@ -1,13 +1,27 @@
 "use client";
 
 import { Button } from '@/components/ui/button'
-import { PlusIcon } from 'lucide-react';
+import { PlusIcon, XCircleIcon } from 'lucide-react';
 import React, { useState } from 'react'
 import { NewAgentDialog } from './new-agent-dialog';
+import { useAgentsFilters } from '../../hooks/use-agents-filters';
+import { AgentsSearchFilter } from './agents-search-filter';
+import { DEFAULT_PAGE } from '@/constants';
 
 export const AgentsListHeader = () => {
 
     const [isDialogOpen,setIsDialogOpen] = useState(false);
+
+    const [filters,setFilters] = useAgentsFilters();
+
+    const isAnyFilterModified = !!filters.search;
+
+    const onClearFilters = () => {
+        setFilters({
+            search: "",
+            page: DEFAULT_PAGE
+        })
+    }
 
     return (
         <>
@@ -19,6 +33,15 @@ export const AgentsListHeader = () => {
                         <PlusIcon/>
                         New Agent
                     </Button>
+                </div>
+                <div className='flex items-center gap-x-2 p-1'>
+                    <AgentsSearchFilter/>
+                    {isAnyFilterModified && (
+                        <Button variant={"outline"} size={"sm"} onClick={onClearFilters}>
+                            <XCircleIcon/>
+                            Clear
+                        </Button>
+                    )}
                 </div>
             </div>
         </>
